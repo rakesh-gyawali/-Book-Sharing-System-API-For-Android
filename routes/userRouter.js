cdd77750147bc7776c15cdff53b08e597e8b23ff
Rakesh <rakeshgyawali@gmail.com>
2020-06-23 16:12:47 +0545

    Add Login with JWT

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
@@ -8,6 +9,7 @@ router.get('/', (req, res, next) => {
 });
 
 //Register:Test Done!!
+//Login: Test Done !!(Jwt token in generated successfully!)
 router.post('/register', (req, res, next) => {
     let {
         username,
@@ -26,20 +28,55 @@ router.post('/register', (req, res, next) => {
             err.status = 401;
             return next(err);
         }
+        
         bcrypt.hash(password, 10, (err, hash) => {
-
             if (err) next(err);
             User.create({username, password: hash, firstName, lastName, address, contact, profilePhoto, role})
             .then(user => {
-                res.json('Status: Registration Successful!  ');
+                res.json('Status: Registration Successful! ');
             }).catch(err);
         }); 
     })
 });
 
-
 router.post('/login', (req, res, next) => {
-    res.send("Login will be performed later!!!");
+    let { username, password } = req.body;
+    User.findOne({username})
+    .then(user => {
+        if(!user) {
+            let err = new Error('User not found!');
+            err.status = 401;
+            return next(err);
+        } 
+
+        bcrypt.compare(password, user.password) //comparing pass from user input and from database.
+        .then(isMatched => {
+            if (!isMatched) {
+                let err = new Error('Password does not match!');
+                err.status = 404;
+                return next(err);
+            }
+            let payload = {
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role
+            }
+            //Token is created here.
+            jwt.sign(payload, process.env.SECRET, (err, token) => {
+                if (err) {
+                    return next(err);
+                }
+                res.json({
+                    status: 'Login Sucessful',
+                    token: `Bearer ${token}`
+                })
+            });
+
+        }).catch(next);
+    
+    }).catch(next);
 })
 
 module.exports = router;
